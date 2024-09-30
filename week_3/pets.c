840d8e55646d4aba1f0f7c6c9bf9550890871531
Nicole <z5206727@unsw.edu.au>
2024-09-30 16:17:24 +1000

    Uploaded tutorial code

--- week_3/pets.c
+++ week_3/pets.c
@@ -5,13 +5,35 @@
 
 // Define an enum for types of animals
 
+#define TRUE 1
+#define FALSE 0
+
+enum animal {
+  DOG,
+  CAT,
+  BIRD,
+  FISH,
+  RABBIT,
+};
+
 // Define a struct for pets
+struct pet {
+  int has_eyes;
+  double weight;
+  enum animal animal_type;
+};
 
 int main(void) {
     
   // Create a pet
+  struct pet lucky;
+  lucky.has_eyes = FALSE;
+  lucky.weight = 10.7;
+  lucky.animal_type = DOG;
 
   // Print it's details
+  printf("Lucky: %d, %lf, %d\n", 
+          lucky.has_eyes, lucky.weight, lucky.animal_type);
 
 	return 0;
 }
